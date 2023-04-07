0afa9e96522e5e965ba803eac4243b22361ef815
Kevin Alejandrino <kgalejandrino@gmail.com>
2023-04-06 20:24:16 -0700

    Added hero section, textbox and socials link. Next: Skills component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Home />
     </Fragment>
   );
 }
