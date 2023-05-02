be833a4d57fa36749daa7fba942835335fc3a75e
Kevin Alejandrino <kgalejandrino@gmail.com>
2023-05-02 13:53:26 -0700

    Fix layout and content for hero section

--- src/components/Hero/HeroTextbox/HeroTextbox.js
+++ src/components/Hero/HeroTextbox/HeroTextbox.js
@@ -3,9 +3,7 @@ import classes from "./HeroTextbox.module.css";
 const HeroTextbox = () => {
   return (
     <div className={classes.textbox}>
-      <p>Hi, I'm</p>
-      <h1 className={classes.name}>Kevin Alejandrino.</h1>
-      {/* <h1 className={classes.title}>Front-end React Developer.</h1> */}
+      <h1 className={classes.name}>Hello there! I'm Kevin.</h1>
       <p className={classes.description}>
         A front-end web developer currently based in California with a passion
         for creating beautiful user interfaces using React.js.
